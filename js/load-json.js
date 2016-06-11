077f12b7944609db77e51f3f8756dd8ef36468b8
widonwaker <hey.lol@live.it>
2016-06-11 11:31:27 +0200

    Update load-json.js

--- js/load-json.js
+++ js/load-json.js
@@ -4,23 +4,23 @@ $(document).ready(function(){
 		onDeviceReady();
 	});
 
-	var output = $('#output');
+	var output = $('.swiper-wrapper');
 
 	$.ajax({
-		url: 'http://gameshift.net/app_test/a.php',
+		url: 'http://gestisciapp.it/gruppodipalo/db_to_app_home.php',
 		dataType: 'jsonp',
 		jsonp: 'jsoncallback',
 		timeout: 5000,
 		success: function(data, status){
 			$.each(data, function(i,item){ 
-				var landmark = '<h1>'+item.nome+'</h1>'
-				+ item.eta+'</p>';
-			
+				var landmark = '<div class="swiper-slide"><img src="http://gestisciapp.it/gruppodipalo/images/'+item.img+'" style="width:100%;"></img></div>';			
 				output.append(landmark);
-			});
+			}); 
+			
 		},
 		error: function(){
 		   output.text('There was an error loading the data.');
 		}
 	});
+	
 });
