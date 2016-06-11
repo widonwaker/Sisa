28c3d899ff35d45cd32a5d64a7e84f2a1022621c
widonwaker <hey.lol@live.it>
2016-06-11 13:37:21 +0200

    Update load-json.js

--- js/load-json.js
+++ js/load-json.js
@@ -3,8 +3,22 @@ $(document).ready(function(){
 		//Phonegap ready
 		onDeviceReady();
 	});
-
+    
+	var start = $('.swiper-container');
 	var output = $('.swiper-wrapper');
+
+    var swiper = new Swiper('.swiper-container', {
+        pagination: '.swiper-pagination',
+        nextButton: '.swiper-button-next',
+        prevButton: '.swiper-button-prev',
+        paginationClickable: true,
+        spaceBetween: 30,
+        centeredSlides: true,
+        autoplay: 2500,
+        autoplayDisableOnInteraction: false,
+		observer: true
+    });
+    start.append(swiper);
 
 	$.ajax({
 		url: 'http://gestisciapp.it/gruppodipalo/db_to_app_home.php',
@@ -15,6 +29,7 @@ $(document).ready(function(){
 			$.each(data, function(i,item){ 
 				var landmark = '<div class="swiper-slide"><img src="http://gestisciapp.it/gruppodipalo/images/'+item.img+'" style="width:100%;"></img></div>';			
 				output.append(landmark);
+
 			}); 
 			
 		},
